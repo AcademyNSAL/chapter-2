dcd031625dffda0f8d19f72137aef4193dec8be2
Hussain Yousuf Sunain <webhys2003@gmail.com>
2024-09-11 19:07:56 +0500

    sum, sub and Modulus operator

--- arthamic-operator.c
+++ arthamic-operator.c
@@ -18,3 +18,65 @@
 //     float a = 3.0 * 3.0;
 //     printf("%f", a);
 // }
+
+// Addition Operator
+
+// #include<stdio.h>
+
+// void main(){
+//     // This is for get the value of subjects and sum is for store the result
+//     int sum, math, science;
+//     // This is for get the marks of math
+//     printf("Enter Marks of Mathematic: ");
+//     scanf("%d", &math);
+//     // This is for get the marks of science
+//     printf("Enter marks of Science: ");
+//     scanf("%d", &science);
+//     // This is for calculate the sum of math and science
+//     sum = math + science;
+//     // This is for print the result
+//     printf("Sum of these two subjects is: %d", sum);
+// }
+
+// subtraction
+
+// #include<stdio.h>
+
+// void main(){
+//     int sub = 10 - 5;
+//     printf("%d", sub);
+// }
+
+// Modulus Operator
+// #include<stdio.h>
+
+// void main(){
+//     float a = 14 % 3;
+//     printf("%f", a);
+// }
+
+// Program Time 2.5
+
+// #include<stdio.h>
+
+// void main(){
+//     int num, dight;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     dight = num % 10;
+//     printf("Right most dight of number you enter is: %d", dight);
+// }
+
+// Activity 2.7
+
+// #include<stdio.h>
+
+// void main(){
+//     int num1, num2, prod;
+//     printf("Enter 1st number: ");
+//     scanf("%d", &num1);
+//     printf("Enter 2nd number: ");
+//     scanf("%d", &num2);
+//     prod = num1 * num2;
+//     printf("Product of two numbers is: \n%d", prod);
+// }
